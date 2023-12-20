e574c8b6c962110a4759ed3f4c12a91dd0991187
Zeref-XXX <72185317+Zeref-XXX@users.noreply.github.com>
2023-12-20 10:10:45 +0530

    added post

---
+++ sheet1/2-divide.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2;
+    cout << "Enter the numbers " << endl;
+    cin >> num1 >> num2;
+    cout << "Quotient : " << num1 / num2 << endl;
+    cout << "Remainder : " << num1 % num2;
+
+    return 0;
+}

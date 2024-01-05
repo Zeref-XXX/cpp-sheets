289c4fc3b462e638527c42d1aabc91ca27af311a
Zeref-XXX <72185317+Zeref-XXX@users.noreply.github.com>
2024-01-05 19:28:29 +0530

    9

--- group-c/sheet3/part-2/8-sum-of-n-odd.cpp
+++ group-c/sheet3/part-2/8-sum-of-n-odd.cpp
@@ -1,22 +1,23 @@
 // 8. Write a program to find the sum of first ‘N’ odd natural numbers without Loop.
 
-
-#include<iostream>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-int terms,n=1,sum=1;
-cout<<"Enter the number of terms ";
-cin>>terms;
 
-while(terms>1){
-    n=n+2;
-    sum +=n;
-    terms--;
+int oddsum(int term)
+{
+    if (term == 0)
+        return 0;
+    else
+        return ((2 * term - 1) + oddsum(term - 1));
 }
 
-
-cout<<" "<<sum;
-return 0;
+int main()
+{
+    int terms, n = 1, sum = 0;
+    cout << "Enter the number of terms ";
+    cin >> terms;
+    sum = oddsum(terms);
+    cout << " " << sum;
+    return 0;
 }
---
+++ group-c/sheet3/part-2/9-product-n.cpp
@@ -0,0 +1,13 @@
+// 9. Write a Program to find the product of first ‘N’ natural Numbers.
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int n;
+cout<<"Enter the number of terms ";
+cin>>n;
+
+return 0;
+}
